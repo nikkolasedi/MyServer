b3042228835600f40caab532777f80fb21e9950c
Nikkolas Edi P <nikkolasep@gmail.com>
2018-08-27 14:52:18 +0200

    Neo4J-Database-Connection added

---
+++ src/myServer/Example.java
@@ -0,0 +1,104 @@
+package myServer;
+import org.neo4j.driver.v1.*;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
+public class Example {
+	 // Driver objects are thread-safe and are typically made available application-wide.
+    Driver driver;
+
+    public Example(String uri, String user, String password)
+    {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    public void addPerson(String name)
+    {
+        // Sessions are lightweight and disposable connection wrappers.
+        try (Session session = driver.session())
+        {
+            // Wrapping Cypher in an explicit transaction provides atomicity
+            // and makes handling errors much easier.
+            try (Transaction tx = session.beginTransaction())
+            {
+                tx.run("MERGE (a:Person {name: {x}})", parameters("x", name));
+                tx.success();  // Mark this write as successful.
+            }
+        }
+    }
+    public void fullCheck(String[] input) {
+    	int n = input.length-1;
+    	int i = 0;
+    	for(i=0;i<=n;i++){
+    		if(input[i]==null) {
+    			break;
+    		}
+    	};
+    	if(2*i>n) {
+    		String[] inputTemp = new String[2*n];
+    		inputTemp = input.clone();
+    		input = inputTemp;
+    	}
+    	
+    }
+
+    public void printPeople(String initial)
+    {
+        try (Session session = driver.session())
+        {
+            // Auto-commit transactions are a quick and easy way to wrap a read.
+            StatementResult result = session.run(
+                    "MATCH (a:Person) WHERE a.name STARTS WITH {x} RETURN a.name AS name",
+                    parameters("x", initial));
+            // Each Cypher execution returns a stream of records.
+            while (result.hasNext())
+            {
+                Record record = result.next();
+                // Values can be extracted from a record by index or name.
+                System.out.println(record.get("name").asString());
+                String[] names = new String[5];
+                
+            }
+        }
+    }
+    public String[] getPeople(String initial)
+    {
+    	String[] names = new String[5];
+        try (Session session = driver.session())
+        {
+            // Auto-commit transactions are a quick and easy way to wrap a read.
+            StatementResult result = session.run(
+                    "MATCH (a:Person) WHERE a.name STARTS WITH {x} RETURN a.name AS name",
+                    parameters("x", initial));
+            // Each Cypher execution returns a stream of records.
+            while (result.hasNext())
+            {
+                Record record = result.next();
+                // Values can be extracted from a record by index or name.
+                String input = record.get("name").asString();
+                fullCheck(names);
+                for(int i = 0; i< names.length; i++) {
+                	if(names[i]!=null) {
+                		
+                	}else {
+                		names[i]=input;
+                		break;
+                	}
+                
+                }
+                
+            }
+        }
+        System.out.println("Successfully running");
+        return names;
+        
+    }
+
+    public void close()
+    {
+        // Closing a driver immediately shuts down all open connections.
+        driver.close();
+    }
+
+   
+}
--- src/myServer/MyServer.java
+++ src/myServer/MyServer.java
@@ -4,13 +4,21 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import org.neo4j.driver.v1.*;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
 
 public class MyServer extends UnicastRemoteObject implements MyInterface {
 	
 	public MyServer() throws RemoteException{
 		
 	}
+	public static String[] dataNames = new String[5];
 	@Override
+	public String[] dataNames() {
+		return dataNames;
+	}
 	public int length(String text) {
 		
 		try {
@@ -21,9 +29,21 @@ public class MyServer extends UnicastRemoteObject implements MyInterface {
 		}
 	}
 public static void main(String[] args) {
+		try {
+		Example example = new Example("bolt://localhost:11002","test","123");
+		example.addPerson("nikko123");
+		example.printPeople("n");
+		dataNames = example.getPeople("n");
+		example.close();
+		}catch(Exception e) {
+			
+		
+			e.printStackTrace();
+		}
+		
 		
 		try {
-			Registry reg = LocateRegistry.createRegistry(1022);
+			Registry reg = LocateRegistry.createRegistry(1023);
 			MyServer c = new MyServer();
 			reg.rebind("myRMI", c);
 			
